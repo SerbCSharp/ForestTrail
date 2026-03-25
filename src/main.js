c7b137e11b08a3cc9614ccef2fc50149b420916c
Serb <SerbCSharp@mail.ru>
2026-03-25 21:11:34 +0300

    Начало

--- src/main.js
+++ src/main.js
@@ -1,12 +1,9 @@
-import * as THREE from 'three';
+import * as THREE from "three";
 
-const scene = new THREE.Scene()
+const scene = new THREE.Scene();
 
-const cubeGeometry = new THREE.BoxGeometry(1,1,1)
-const cubeMaterial = new THREE.MeshBasicMaterial({color: "red"})
-const cubeMesh = new THREE.Mesh(
-  cubeGeometry,
-  cubeMaterial
-)
+const cubeGeometry = new THREE.BoxGeometry(1, 1, 1);
+const cubeMaterial = new THREE.MeshBasicMaterial({ color: "red" });
+const cubeMesh = new THREE.Mesh(cubeGeometry, cubeMaterial);
 
-scene.add(cubeMesh)
+scene.add(cubeMesh);
