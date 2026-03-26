87cc66f0d9086f8b20a72d19e27ccebff8579179
SerbCSharp <SerbCSharp@mail.ru>
2026-03-26 16:12:00 +0300

    Перешел на стартовые наборы

--- src/main.js
+++
@@ -1,9 +0,0 @@
-import * as THREE from "three";
-
-const scene = new THREE.Scene();
-
-const cubeGeometry = new THREE.BoxGeometry(1, 1, 1);
-const cubeMaterial = new THREE.MeshBasicMaterial({ color: "red" });
-const cubeMesh = new THREE.Mesh(cubeGeometry, cubeMaterial);
-
-scene.add(cubeMesh);
