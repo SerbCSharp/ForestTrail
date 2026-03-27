84f053e866b952d2885da7ff374551c45d74ecf6
SerbCSharp <SerbCSharp@mail.ru>
2026-03-27 14:11:38 +0300

    Add import Orbit

--- src/script.js
+++ src/script.js
@@ -1,4 +1,7 @@
 import * as THREE from 'three';
+import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
+
+console.log(OrbitControls)
 
 // initialize the scene
 const scene = new THREE.Scene()
@@ -6,25 +9,15 @@ const scene = new THREE.Scene()
 // add objects to the scene
 const cubeGeometry = new THREE.BoxGeometry(1,1,1)
 const cubeMaterial = new THREE.MeshBasicMaterial({color: "red"})
-
-const cubeMesh = new THREE.Mesh(
-  cubeGeometry,
-  cubeMaterial
-)
+const cubeMesh = new THREE.Mesh(cubeGeometry, cubeMaterial)
 scene.add(cubeMesh)
 
 // initialize the camera
-const camera = new THREE.PerspectiveCamera(
-  75, 
-  window.innerWidth / window.innerHeight,
-  0.1,
-  30)
+const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 30)
 camera.position.z = 5
 
 // initialize the renderer
 const canvas = document.querySelector('canvas.threejs')
-const renderer = new THREE.WebGLRenderer({
-  canvas: canvas
-})
+const renderer = new THREE.WebGLRenderer({canvas: canvas})
 renderer.setSize(window.innerWidth, window.innerHeight)
 renderer.render(scene, camera)
